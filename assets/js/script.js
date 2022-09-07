cdc921eafdf3c2296882038d2c71d5c13e1a6731
Wanessa Santos <wanessa.ufpe2018@gmail.com>
2022-09-07 00:08:55 -0300

    :sparkles: menu sidebar

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var hamburguer = document.querySelector(".hamburguer");
+hamburguer.addEventListener("click", function(){
+  var sidebar = document.querySelector(".sidebar").classList.toggle("show-menu");
+})
